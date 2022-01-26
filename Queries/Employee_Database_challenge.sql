2d82f7529901071da4f5d362ebf7e1d741aa6daf
REVATI KULKARNI <revatiikatkar@yahoo.com>
2022-01-25 22:31:08 -0500

    code files of Pewlett-Hackard-Analysis
    All sql files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,74 @@
+--Deliverable 1
+--Retirement Titles table for employees who are born between January 1, 1952 and December 31, 1955.
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+-- Employee count by department number
+SELECT distinct title FROM unique_titles;
+
+--create a Retiring Titles table that contains the number of titles filled by employees who are retiring
+CREATE TABLE retiring_titles(
+	count bigint,
+	title VARCHAR(50)
+);
+
+SELECT * FROM retiring_titles;
+
+SELECT count(emp_no),title
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+--Deliverable 2
+SELECT emp_no,
+first_name,
+last_name,
+birth_date
+FROM employees;
+
+SELECT from_date,
+to_date
+FROM dept_emp;
+
+SELECT title FROM titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT distinct on(e.emp_no)e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+    de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
